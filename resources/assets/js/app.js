5d4c8225deb6d47c374b39fa89372c7bf109049d
tanh123678 <tanh123678@gmail.com>
2019-04-11 14:14:51 +0700

    first commit

---
+++ resources/assets/js/app.js
@@ -0,0 +1,77 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+/**
+ * The following block of code may be used to automatically register your
+ * Vue components. It will recursively scan this directory for the Vue
+ * components and automatically register them with their "basename".
+ *
+ * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ */
+
+// const files = require.context('./', true, /\.vue$/i);
+// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+
+
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+import App from './App.vue';
+import Dashboard from './components/Dashboard.vue';
+import Home from './components/Home.vue';
+import Register from './components/Register.vue';
+import Login from './components/Login.vue';
+Vue.use(VueRouter);
+Vue.use(VueAxios, axios);
+axios.defaults.baseURL = 'http://localhost:8000/api';
+const router = new VueRouter({
+    routes: [{
+        path: '/',
+        name: 'home',
+        component: Home
+    },{
+        path: '/register',
+        name: 'register',
+        component: Register,
+        meta: {
+            auth: false
+        }
+    },{
+        path: '/login',
+        name: 'login',
+        component: Login,
+        meta: {
+            auth: false
+        }
+    },{
+        path: '/dashboard',
+        name: 'dashboard',
+        component: Dashboard,
+        meta: {
+            auth: true
+        }
+    }]
+});
+Vue.router = router
+Vue.use(require('@websanova/vue-auth'), {
+   auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
+   http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
+   router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
+});
+App.router = Vue.router
+new Vue(App).$mount('#app');
